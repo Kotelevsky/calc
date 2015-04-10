10f89478fac532bb2a2592cb45e15387ea6d810c
Kotelevsky Dmitry <kotelevskiydm@gmail.com>
2015-04-11 00:41:18 +0300

    Created structure of app

---
+++ app/src/main/java/com/dmitriy/emp/calcpreject/CalcActivity.java
@@ -0,0 +1,15 @@
+package com.dmitriy.emp.calcpreject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Dmitriy on 11.04.2015.
+ */
+public class CalcActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calc_layout);
+    }
+}
