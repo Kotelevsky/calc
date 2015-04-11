c105d3fa0d2f1925e8119910a0a028980e66746c
Kotelevsky Dmitry <kotelevskiydm@gmail.com>
2015-04-11 16:38:10 +0300

    init Application

--- app/src/main/java/com/dmitriy/emp/calcpreject/CalcActivity.java
+++ app/src/main/java/com/dmitriy/emp/calcpreject/CalcActivity.java
@@ -2,14 +2,33 @@ package com.dmitriy.emp.calcpreject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by Dmitriy on 11.04.2015.
  */
 public class CalcActivity extends Activity {
+
+    public static final String EMPTY_STRING = new String();
+    private TextView txtEdit;
+    private TextView txtEqual;
+    private TextView txtResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calc_layout);
+
+        init();
+    }
+
+    private void init() {
+        txtEdit = (TextView) findViewById(R.id.txtEdit);
+        txtEqual = (TextView) findViewById(R.id.txtEquals);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+
+        txtEdit.setText(EMPTY_STRING);
+        txtEqual.setText(EMPTY_STRING);
+        txtResult.setText(EMPTY_STRING);
     }
 }
